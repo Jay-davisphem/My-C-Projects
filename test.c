808b45b9b5ee5fffc27e5a85c7697f20fdc251f6
David Oluwafemi <davidoluwafemi178@gmail.com>
2021-01-28 23:50:17 +0100

    add simple DBMS program

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char * argv[]){
+/*int main(int argc, char * argv[]){
   int arr[] = {76, 45, 93, 4, 6, 4, 45, 5};
   char rt[5] = {'r', 'i', 'y', 'o', '\0'};
   int *arr_ptr = arr;
@@ -8,4 +8,33 @@ int main(int argc, char * argv[]){
   int aba = 56;
   int *ptaba = &aba;
   printf("%d %p\n", *ptaba, ptaba);
+  uint32_t i;
+  scanf("%u", &i);
+  printf("%u\n", ~i);
+
+  FILE *cur;
+  
+  fopen(filename, "r+");
+  fputs("Oluwafemi Boy.\nI love you o.", cur);
+  printf("%d %s\n", TMP_MAX, filename);*/
+
+  static int j;
+  static int k;
+  void up(){
+    j++;
+    k++;
+
+    printf("up() called.   k= %2d, j= %2d\n", k , j);
+  }
+
+  void down(){
+    j--;
+    k--;
+
+    printf("down() called. k= %2d, j= %2d\n", k , j);
+  }
+int main(void){
+  int i = 0;
+  for (i = 0; i < 3; i++) up();
+  for (i = 0; i < 2; i++) down();
 }
